283336edb619dc59595197f607aaf78927e1912f
yhwa <hfbc93@gmail.com>
2023-05-24 18:57:19 +0900

    Feat: Base 레이아웃 include로 컴포넌트별 정리

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,2 +1,24 @@
 import "../scss/styles.scss";
-console.log("hey")
+
+const KEY_NAME = "poong_theme";
+const theme = localStorage.getItem(KEY_NAME) || "darkMode";
+document.documentElement.setAttribute("data-theme", theme);
+localStorage.setItem(KEY_NAME, theme);
+
+const themeToggleBtn = document.getElementById("themeToggleBtn");
+themeToggleBtn.className = theme === "darkMode" ? "darkMode" : "lightMode";
+
+const toggleTheme = (event) => {
+  if (event.target.className === "darkMode"){
+    document.documentElement.setAttribute("data-theme", "lightMode");
+    event.target.className = "lightMode";
+    localStorage.setItem(KEY_NAME, "lightMode");
+
+  } else if (event.target.className === "lightMode"){
+    document.documentElement.setAttribute("data-theme", "darkMode");
+    event.target.className = "darkMode";
+    localStorage.setItem(KEY_NAME, "darkMode");
+  }
+}
+
+themeToggleBtn.addEventListener("click", toggleTheme);
