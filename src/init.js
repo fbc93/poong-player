f79e01adfbc415fe7e2cf75301bd2de5b5fc0648
yhwa <hfbc93@gmail.com>
2023-06-06 17:08:27 +0900

    Deploy: init 수정

--- src/init.js
+++ src/init.js
@@ -6,5 +6,4 @@ import app from "./server";
 const PORT = process.env.PORT || 4000;
 
 //서버의 외부접속 listen...
-app.listen(4000, () => 
-  console.log(`1. ✅ Server Listening on Port : http://localhost:${PORT} 🎉`));
+app.listen(4000, () => console.log(`1. ✅ Server Listening on Port : http://localhost:${PORT} 🎉`));
