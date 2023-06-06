f09877ff99117cbbe30840653ad65f63fce74b5d
yhwa <hfbc93@gmail.com>
2023-06-06 17:46:42 +0900

    Deploy: port 누락 수정

--- src/init.js
+++ src/init.js
@@ -6,4 +6,4 @@ import app from "./server";
 const PORT = process.env.PORT || 4000;
 
 //서버의 외부접속 listen...
-app.listen(4000, () => console.log(`1. ✅ Server Listening on Port : http://localhost:${PORT} 🎉`));
+app.listen(PORT, () => console.log(`1. ✅ Server Listening on Port : http://localhost:${PORT} 🎉`));
